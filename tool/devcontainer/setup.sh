72122958192374d3b35a794cdb9dd20038418d1b
Chykon <chykon1@proton.me>
2023-08-31 19:42:30 +0300

    Add GitHub Codespaces (#16)

---
+++ tool/devcontainer/setup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Add Dart repository key.
+
+declare -r input_pubkey_file='tool/devcontainer/dart.pub'
+declare -r output_pubkey_file='/usr/share/keyrings/dart.gpg'
+
+sudo gpg --output ${output_pubkey_file} --dearmor ${input_pubkey_file}
+
+# Add Dart repository.
+
+declare -r dart_repository_url='https://storage.googleapis.com/download.dartlang.org/linux/debian'
+declare -r dart_repository_file='/etc/apt/sources.list.d/dart_stable.list'
+
+echo "deb [signed-by=${output_pubkey_file}] ${dart_repository_url} stable main" | sudo tee ${dart_repository_file}
+
+# Update the list of available packages.
+
+sudo apt-get update
+
+# Install Dart SDK.
+
+sudo apt-get install dart
+
+# Get project dependencies.
+
+dart pub get
+
+# Install Icarus Verilog.
+
+sudo apt-get install iverilog
